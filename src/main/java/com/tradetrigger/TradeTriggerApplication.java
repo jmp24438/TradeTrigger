aaa62a41c829f1ee43d92bc3fc593edc84884d6c
Jay Patel <60836234+jmp24438@users.noreply.github.com>
2025-06-28 01:18:44 -0400

    init

---
+++ src/main/java/com/tradetrigger/TradeTriggerApplication.java
@@ -0,0 +1,13 @@
+package com.tradetrigger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeTriggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeTriggerApplication.class, args);
+	}
+
+}
